1672fe2e4b9df1ceb9306d819773b5d55736172c
Vinni Hoke <vinni.hoke@gmail.com>
2019-08-15 11:06:33 -0700

    Fixing Structure

---
+++ src/App.js
@@ -0,0 +1,37 @@
+// React Imports
+import React, { useState, useEffect } from 'react';
+
+// Material-UI
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+
+// Local
+import Data from './data/Data';
+import Navbar from './components/Nav/Navbar';
+import TeamDisplay from './components/TeamDisplay';
+import Form from './components/Form';
+import Hero from './components/Hero';
+
+const App = () => {
+  const [members, setMembers] = useState(Data);
+
+  const addMember = member => {
+    setMembers([...members, member]);
+  }
+
+  return (
+    <>
+    <CssBaseline />
+    <Navbar />
+    <Hero />
+    <Container className="App">
+      <Form addMember={addMember}/>
+      <TeamDisplay members={members}/>
+    </Container>
+    </>
+  );
+}
+
+export default App;
